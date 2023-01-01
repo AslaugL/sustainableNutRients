027c435766c4857aa73b01eff08dde210b4a05c6
AslaugL <63227797+AslaugL@users.noreply.github.com>
2023-01-01 22:26:59 +0100

    Create matvaretabellen_units.R

---
+++ data-raw/matvaretabellen_units.R
@@ -0,0 +1,22 @@
+#Units from Matvaretabellen
+matvaretabellen_units <- tibble(
+  'nutrient' = c('Vitamin A', 'Retinol', 'Beta-carotene',
+                'Vitamin D','Vitamin E', 'Thiamin',
+                'Riboflavin','Niacin', 'Vitamin B6',
+                'Folate', 'Vitamin B12', 'Vitamin C',
+                'Calcium', 'Iron', 'Sodium',
+                'Potassium', 'Magnesium', 'Zinc',
+                'Selenium', 'Copper', 'Phosphorus',
+                'Iodine'),
+
+  'unit' = c("mcg-RE", "mcg", "mcg",
+             "mcg", "mg-ATE", "mg",
+             "mg", "mg", "mg",
+             "mcg", "mcg", "mg",
+             "mg", "mg", "mg",
+             "mg", "mg", "mg",
+             "mcg", "mg", "mg",
+             "mcg")
+)
+
+saveRDS(matvaretabellen_units,"./data-raw/matvaretabellen_units.Rds")
