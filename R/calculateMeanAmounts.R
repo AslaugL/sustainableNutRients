7f9052b0c6080320c6104d118d4b4155860a6e3a
AslaugL <63227797+AslaugL@users.noreply.github.com>
2023-01-13 23:56:48 +0100

    Recognize decimal points

--- R/calculateMeanAmounts.R
+++ R/calculateMeanAmounts.R
@@ -11,16 +11,16 @@
 calculateMeanAmounts <- function(df) {
 
   #Check if means need to be calculated
-  if(isTRUE(any(str_detect(df$Ingredients, "\\d+-\\d+|\\d+ to \\d+")))) {
+  if(isTRUE(any(str_detect(df$Ingredients, "(\\d+\\.\\d+|\\d+)-(\\d+\\.\\d+|\\d+)|(\\d+\\.\\d+|\\d+) to (\\d+\\.\\d+|\\d+)")))) {
 
     #Find the pattern
     numerics <- df %>%
-      mutate(tmp = str_extract(Ingredients, "\\d+-\\d+|\\d+ to \\d+")) %>%
+      mutate(tmp = str_extract(Ingredients, "(\\d+\\.\\d+|\\d+)-(\\d+\\.\\d+|\\d+)|(\\d+\\.\\d+|\\d+) to (\\d+\\.\\d+|\\d+)")) %>%
       #Separate it
       select(tmp) %>%
       unique() %>%
       #calculate mean of the two numbers
-      separate(., tmp, c("no1", "no2"), sep = "-", remove = FALSE) %>%
+      separate(., tmp, c("no1", "no2"), sep = "-| to ", remove = FALSE) %>%
       mutate(across(c(no1, no2), ~as.numeric(.))) %>%
       mutate(mean = as.character((no1 + no2)/2)) %>% #Character as Ingredients string is character
       drop_na()
