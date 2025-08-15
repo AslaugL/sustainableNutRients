ee75bd3cfee02135feceed7bbfe76e2545d899a0
AslaugL <63227797+AslaugL@users.noreply.github.com>
2025-08-15 07:15:54 +0200

    Added a control mechanism to check format

--- R/fixFoodMappingError.R
+++ R/fixFoodMappingError.R
@@ -13,6 +13,21 @@
 
 fixFoodMappingError <- function(database, first_w, second_w = '\\') {
 
-  ID <- database %>% filter(first_word == first_w & second_word == second_w) %>% select(database_ID) %>% unique() %>% as.numeric()
+  ID <- database %>% filter(first_word == first_w & second_word == second_w) %>%
+    select(database_ID) %>% distinct() %>% pull(database_ID)
+
+  if(is.list(ID)){
+    print(paste0("More than one ID discovered when trying to fix ", first_w, " and ", second_w, ": ", ID))
+    stop()
+
+  } else if(isTRUE(ID == 0)) {
+    print(paste0("ID of ", first_w, " and ", second_w, " is supposedly zero. ID: ", ID))
+
+
+  } else {
+
+    as.numeric(ID)
+
+  }
 
 }
